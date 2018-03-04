27a105590bb232cf571d5d05cbf6577bbdc6ace5
Brian <ballerbrian4@gmail.com>
2018-03-03 23:40:46 -0800

    ch 10 complete

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -4,29 +4,29 @@
   //checks to see if the App property is present to assign, or create new object if not present
   var App = window.App || {};
 
-  function Datastore(){
+  function DataStore(){
     //console.log("running the Datastore function");
     this.data = {};
   }
   //function is accessible everytime an instance is created/ function is accessible
-  Datastore.prototype.add = function (key, val){
+  DataStore.prototype.add = function (key, val){
     this.data[key] = val;
   };
 
   //function to get individual datra
-  Datastore.prototype.get = function (key){
+  DataStore.prototype.get = function (key){
     return this.data[key];
   };
   //function to return multiple data
-  Datastore.prototype.getAll = function(){
+  DataStore.prototype.getAll = function(){
     return this.data;
   };
 
-  Datastore.prototype.remove = function(key){
+  DataStore.prototype.remove = function(key){
     delete this.data[key];
   };
 
-  App.Datastore = Datastore;
+  App.DataStore = DataStore;
   window.App = App;
 
 })(window);
